14bed6beaf015700bfd307ecf28e4a47b8d594b1
ENG Chanveasna <2023488eng@aupp.edu.kh>
2025-11-25 06:50:22 +0700

    Fix: requirements

--- auth/auth.py
+++ auth/auth.py
@@ -3,8 +3,8 @@ from typing import Annotated
 from fastapi import APIRouter, Depends, HTTPException, status
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
-from database import SessionLocal
-from models import Users
+from auth.database import SessionLocal
+from auth.models import Users
 from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from jose import jwt, JWTError
--- auth/models.py
+++ auth/models.py
@@ -1,4 +1,4 @@
-from database import Base 
+from auth.database import Base 
 from sqlalchemy import Column, Integer, String 
 
 class Users(Base):
