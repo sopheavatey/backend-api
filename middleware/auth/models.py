8193bd8b31885f471e5e64f11387994cf99f67f2
Chanveasna ENG <2023488eng@aupp.edu.kh>
2025-11-25 11:45:47 +0700

    fix

--- middleware/auth/models.py
+++ middleware/auth/models.py
@@ -7,4 +7,4 @@ class Users(Base):
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True)
     email = Column(String, unique=True)
-    hashed_password = Column(String)
+    password_hash = Column(String)
